17f4b05b8796584e8c2ac627a6d8f8541efb2f62
Alex Lapshin <lapshinbz@gmail.com>
2018-10-21 14:22:52 +0700

    Логирование и кнопка старт

--- bot.py
+++ bot.py
@@ -2,16 +2,18 @@ import logging
 import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-#logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                #    level=logging.INFO,
-                #    filename='bot.log')
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 def start_bot(bot, update):
     mytext = " Привет {}!Я простой бот и понимаю только команду /start".format(update.message.chat.first_name)
+    logging.info('Пользователь {} нажал /start'.format(update.message.chat.username))
     update.message.reply_text(mytext)
 
 
-def chat1(bot, update):
+def chat(bot, update):
     text = update.message.reply_text
     logging.info('text')
     update.message.reply_text(text)
@@ -22,12 +24,12 @@ def main():
     updtr = Updater(settings.TELEGRAM_API_KEY)
 
     updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
-    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat1))
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
 
 
     updtr.start_polling()
     updtr.idle()
 
 if __name__ == "__main__":
-    #logging.info('Bot started')
+    logging.info('Bot started')
     main()
